325f18678a7969934d68c4f2ae85df40d10e1b11
Danilyer <danilsirotalol@gmail.com>
2018-08-31 16:15:31 +0300

    prev/next blog link

--- single/single-cat-blog.php
+++ single/single-cat-blog.php
@@ -59,7 +59,7 @@
     </script>
        <!-- <div class="blog-article-prev"></div> -->
        <div class="blog-article-controls">
-       <a href="#" class="blog-article-prev">
+       <a href="<?= get_permalink(get_previous_post()) ?>" class="blog-article-prev">
          <svg version="1.1"  viewBox="0 0 477.175 477.175" style="enable-background:new 0 0 477.175 477.175;" xml:space="preserve">
             <g><path d="M145.188,238.575l215.5-215.5c5.3-5.3,5.3-13.8,0-19.1s-13.8-5.3-19.1,0l-225.1,225.1c-5.3,5.3-5.3,13.8,0,19.1l225.1,225
                 c2.6,2.6,6.1,4,9.5,4s6.9-1.3,9.5-4c5.3-5.3,5.3-13.8,0-19.1L145.188,238.575z"></path>
@@ -67,7 +67,7 @@
       </svg>
        <span>Предидущая статья</span>
        </a>
-              <a href="#" class="blog-article-next">
+              <a href="<?= get_permalink(get_next_post()) ?>" class="blog-article-next">
          <span>Следующая статья</span>
 
           <svg version="1.1" id="next-svg" viewBox="0 0 477.175 477.175" style="enable-background:new 0 0 477.175 477.175;" xml:space="preserve">
